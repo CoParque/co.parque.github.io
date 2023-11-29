16b9d285ae1848fb574862e6143aaad7a9bcfc63
abelbour <abelbour@users.noreply.github.com>
2023-11-29 20:07:17 -0300

    Update config.js

--- app/js/config.js
+++ app/js/config.js
@@ -17,6 +17,7 @@
     window.__env.salidasUrl2 = '/pubhtml?gid=0&single=false&widget=false&headers=false&chrome=false';
     window.__env.salidas2Url1 = '//docs.google.com/spreadsheets/d/';
     window.__env.salidas2Url2 = '';
+    window.__env.gruposUrl = '//docs.google.com/spreadsheets/d/1FwIPqxOl6quPy4aVqi-byFQ0PU6vyeX4iunwi3P9MBg/pubhtml?gid=0&single=false&widget=false&headers=false&chrome=false'; //https://docs.google.com/spreadsheets/d/1FwIPqxOl6quPy4aVqi-byFQ0PU6vyeX4iunwi3P9MBg/edit?usp=sharing
     window.__env.telefonicaIdSheets = '1JuiB_XVz-jO_0Yrzu82pF2d3cbWkDhBwhcp5wF2lOrA';
     window.__env.telefonicaUrl1 = '//docs.google.com/spreadsheets/d/';
     window.__env.telefonicaUrl2 = '/pubhtml?gid=0&single=false&widget=false&headers=false&chrome=false';
